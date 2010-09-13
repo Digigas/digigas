9fc5ab6946c6dadb56325285449ef8dab1d056c6
Alessandro Falezza <alessandro.f@ideaedi.it>
2010-09-13 23:50:45 +0200

    aggiornamento ckeditor

--- app/webroot/js/ckeditor/config.js
+++ app/webroot/js/ckeditor/config.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -11,6 +11,7 @@ CKEDITOR.editorConfig = function( config )
 
         config.extraPlugins='inserthtml';
 
+		config.language = 'it';
 
         config.toolbar = 'Full';
 
@@ -19,10 +20,12 @@ CKEDITOR.editorConfig = function( config )
             ['Maximize', 'ShowBlocks', 'Source','-','Templates'],
             ['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker', 'Scayt'],
             ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+            '/',
             ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
             ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
             ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
             ['Link','Unlink','Anchor'],
+            '/',
             ['Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','CreateDiv','inserthtml'],
             ['Styles','Format','Font','FontSize'],
             ['TextColor','BGColor'],
@@ -33,5 +36,12 @@ CKEDITOR.editorConfig = function( config )
         [
             ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-','About']
         ];
+        
+        config.toolbar_Frontend =
+        [
+            ['Bold', 'Italic', 'Underline','Strike'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+            ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo'],
+            ['Find','Replace','-','SelectAll','RemoveFormat']
+        ];
 };
-
