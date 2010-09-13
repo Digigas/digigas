0f5dea2d818b5dae747309920ad9810772946892
Alessandro Falezza <alessandro.f@ideaedi.it>
2010-09-14 01:17:34 +0200

    stampa ordini in pdf - prima versione

---
+++ app/vendors/pdf.php
@@ -0,0 +1,53 @@
+<?php
+
+if(!class_exists('FPDF')) {
+    App::import('Vendor', 'FPDF', array('file' => 'fpdf'.DS.'fpdf.php'));
+    $PDF = new FPDF();
+}
+
+class PDF extends FPDF {
+
+    function header() {
+        $this->setCreator('Digigas3 - www.digigas.org');
+        $this->SetFont('Arial','',12);
+        $this->Image(WWW_ROOT.'img'.DS.'digigas.jpg', 150, 10, 50, 10);
+        }
+
+    function footer() {
+        $this->SetFont('Arial','',9);
+        $this->Cell(0,0,'Digigas3');
+    }
+
+    function adaptiveCell($actualsize, $w ="", $h="" , $txt="", $border="" , $ln="" ,$align="" , $fill="" , $link="") {
+        $size=$actualsize;
+        if($w) {
+            $this->SetFont('','',$size);
+            while($this->GetStringWidth($txt)>$w) {
+                $size -= .1;
+                $this->SetFont('','',$size);
+            }
+            $this->Cell($w, $h, $txt, $border, $ln, $align, $fill, $link);
+        }
+        $this->SetFont('','',$actualsize);
+    }
+
+    function h1($text) {
+        $this->Ln();
+        $this->SetFont('Arial','B',12);
+        $this->SetTextColor(255, 153, 0);
+        $this->Cell(0, 15, $text);
+        $this->Ln();
+        $this->SetFont('Arial','',9);
+        $this->SetTextColor(0, 0, 0);
+    }
+
+    function table($data) {
+        foreach ($data as $row) {
+            $numCol = count($row);
+            foreach($row as $col) {
+                
+            }
+        }
+    }
+    
+}
