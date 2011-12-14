bae92a3ad8256ab654f470c718fdf1e100fab3f7
Alessandro Falezza <alessandro.f@ideaedi.it>
2011-12-14 10:45:18 +0100

    [no commit message]

--- app/models/user.php
+++ app/models/user.php
@@ -128,7 +128,7 @@ class User extends AppModel {
 		return $return;
 	}
 
-	/*
+		/*
 		 * Funzioni per accedere a $controller->Auth->user() anche nei modelli
 		 *	in AppController::beforeFilter:
 			App::import('Model', 'User');
